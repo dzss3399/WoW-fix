2fad03c66c373f315a753f1830758cd8ea7d4949
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-01-31 17:32:44 +0330

    Update warpauto.php

--- warpauto.php
+++ warpauto.php
@@ -1,15 +1,15 @@
 <?php
 
-    $getList = file_get_contents('https://raw.githubusercontent.com/arshiacomplus/WoW/main/warpauto.json?v1.'.time());
+    $getList = file_get_contents('https://raw.githubusercontent.com/arshiacomplus/WoW-fix/refs/heads/main/warpauto.json?v1.'.time());
     $strings = explode("\n", $getList);
 
     $warp = "//profile-title: base64:44CY4oC0yrfhtYPKs+G1luKAt+OAmfCThILwk4aD\n";
-    $warp .= "//profile-update-interval: 1\n";
-    $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
+    $warp .= "//profile-update-interval: 4\n";
+    $warp .= "//subscription-userinfo: upload = 805306368000; download = 2576980377600; total = 6012954214400; expire = 1762677732\n";
     $warp .= "//profile-web-page-url: https://github.com/arshiacomplus\n\n";
     $warp .= "warp://auto#mansor427🇮🇷 &&detour=warp://auto#@arshiacomplus🇩🇪";
     $warp .= "\n";
-    $warp .= "warp://@auto/?ifp=5-10#🇮🇷𓄂𓆃 &&detour=warp://@auto/?ifp=5-10#🇩🇪@arshiacomplus";
+    $warp .= "warp://@auto/?ifp=1-3&ifpm=m4#🇮🇷𓄂𓆃 &&detour=warp://@auto/?ifp=1-2&ifpm=m5#🇩🇪@arshiacomplus";
     $warp .= "\n";
 
    $i = 1;
